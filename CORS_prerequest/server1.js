7016322a7c0db737111575637c42b3fdfd010dd2
Jsonghh <jiesonghe@outlook.com>
2019-04-03 15:38:58 -0400

    2nd commit

---
+++ CORS_prerequest/server1.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer(function (request, response) {
+    console.log('request comes', request.url);
+    
+    const html = fs.readFileSync('ethel.html', 'utf8')
+    // content-type is 'text/html' by default
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    response.end(html);
+}).listen(8888);
+
---
+++ CORS_prerequest/server2.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+http.createServer(function (request, response) {
+    console.log('request comes', request.url);
+    response.writeHead(200, {
+        'Content-Type': 'text/html',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'X-Test-Cors'
+    });
+    response.end('123');
+
+}).listen(8887);
+
+console.log('server listening on 8887');
